c5bc4dd8c3b44878408e3faa83731e0d5bb8ef5b
Baerdon <jardabazant@seznam.cz>
2017-08-08 15:23:35 +0200

    viewDidAppear
    code switched into viewDidAppear

--- ScrollPage/ViewController.swift
+++ ScrollPage/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //print("Count: \(images.count)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
             let imageView = UIImageView(image: image)
@@ -31,8 +35,7 @@ class ViewController: UIViewController {
             imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height/2) - 75, width: 150, height: 150)
         }
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
-        //print("Count: \(images.count)")
-    }
 
+    }
 }
 
